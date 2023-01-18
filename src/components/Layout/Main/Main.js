952d43e7cc68f0c5650e14834791e29ac2b2dcbe
Rio Edwards <rioredwards@gmail.com>
2023-01-18 15:19:53 -0800

    Add filtering by continent

--- src/components/Layout/Main/Main.js
+++ src/components/Layout/Main/Main.js
@@ -1,18 +1,35 @@
+import { useState } from 'react';
 import { Alert, Container, Row } from 'react-bootstrap';
 import { useCountries } from '../../../hooks/useCountries.js';
 import CountryCard from '../../CountryCard/CountryCard.js';
 
 export default function Main() {
   const { countries, error } = useCountries();
+  const [continent, setContinent] = useState('all');
+  const continents = [
+    ...new Set(countries.filter(({ continent }) => continent).map(({ continent }) => continent)),
+  ];
+
+  const filtered = countries.filter(
+    (country) => country.continent === continent || continent === 'all'
+  );
 
   return (
     <main>
       <Alert show={!!error} className="mx-4" variant="danger">
         {error}
       </Alert>
+      <select onChange={(e) => setContinent(e.target.value)}>
+        <option value="all">all</option>
+        {continents.map((continent) => (
+          <option key={continent} value={continent}>
+            {continent}
+          </option>
+        ))}
+      </select>
       <Container fluid className="d-flex my-3">
         <Row className="px-lg-20">
-          {countries.map((country) => (
+          {filtered.map((country) => (
             <CountryCard key={country.id} {...country} />
           ))}
         </Row>
