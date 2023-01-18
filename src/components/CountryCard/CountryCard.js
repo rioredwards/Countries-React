7456ae7a451a4a1bc3e77b8e85044deec9e527ef
Rio Edwards <rioredwards@gmail.com>
2023-01-17 19:23:37 -0800

    Styling for country cards

--- src/components/CountryCard/CountryCard.js
+++ src/components/CountryCard/CountryCard.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Col } from 'react-bootstrap';
+import { Col, Card } from 'react-bootstrap';
 
 export default function CountryCard({ name, iso2 }) {
   return (
     <Col sm={6} md={4} lg={4} xl={3}>
-      <div>{name}</div>
-      <img
-        src={`https://flagcdn.com/216x162/${iso2.toLowerCase()}.png`}
-        width="64"
-        alt="South Africa"
-      ></img>
+      <Card className="my-2" bg="light" style={{ width: '12rem', height: '12rem' }}>
+        <Card.Header className="text-center">{name}</Card.Header>
+        <Card.Body className="d-flex justify-content-center align-items-center">
+          <Card.Img
+            style={{ width: '8rem', maxHeight: '6rem' }}
+            src={`https://flagcdn.com/216x162/${iso2.toLowerCase()}.png`}
+          />
+        </Card.Body>
+      </Card>
     </Col>
   );
 }
