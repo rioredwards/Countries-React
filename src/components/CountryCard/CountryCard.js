f6737d15970334bca5a28a6ae5eb988c8e8a3bab
Rio Edwards <rioredwards@gmail.com>
2023-01-17 18:57:11 -0800

    Include flag images from flagpedia API

--- src/components/CountryCard/CountryCard.js
+++ src/components/CountryCard/CountryCard.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
 
-export default function CountryCard({ name }) {
+export default function CountryCard({ name, iso2 }) {
   return (
     <Col sm={6} md={4} lg={4} xl={3}>
       <div>{name}</div>
+      <img
+        src={`https://flagcdn.com/216x162/${iso2.toLowerCase()}.png`}
+        width="64"
+        alt="South Africa"
+      ></img>
     </Col>
   );
 }
